11376a91f88904edb1bb1d872fb7615182c87d0c
gotoel <ghostaholic_1@hotmail.com>
2016-01-24 04:23:50 -0500

    First commit.
    Server/Client in the same Visual Studio solution.

---
+++ CCSURAT-Client/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CCSURAT_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.cmdTextbox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // console
+            // 
+            this.console.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.console.BackColor = System.Drawing.Color.Black;
+            this.console.ForeColor = System.Drawing.Color.Lime;
+            this.console.Location = new System.Drawing.Point(-1, 0);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(537, 229);
+            this.console.TabIndex = 0;
+            this.console.Text = "";
+            // 
+            // cmdTextbox
+            // 
+            this.cmdTextbox.Location = new System.Drawing.Point(7, 230);
+            this.cmdTextbox.Name = "cmdTextbox";
+            this.cmdTextbox.Size = new System.Drawing.Size(448, 20);
+            this.cmdTextbox.TabIndex = 1;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(458, 230);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 21);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 254);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.cmdTextbox);
+            this.Controls.Add(this.console);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox console;
+        private System.Windows.Forms.TextBox cmdTextbox;
+        private System.Windows.Forms.Button sendButton;
+    }
+}
+
